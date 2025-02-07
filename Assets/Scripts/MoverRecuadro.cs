e4da0bfda13567f38dfea4b3caaac988d4860fba
Alvaro Morel <avaromorel56@gmail.com>
2025-02-07 00:55:40 -0400

    Descripción de tus cambios

---
+++ Assets/Scripts/MoverRecuadro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverRecuadro : MonoBehaviour
+{
+    public float velocidad = 5f; // Velocidad de movimiento
+   // public float velocidad = 3f; // Velocidad de movimiento
+    private int direccion = 1; // Dirección inicial
+
+    void Update()
+    {
+        // Mover el objeto de izquierda a derecha
+        transform.Translate(Vector3.right * velocidad * Time.deltaTime * direccion);
+
+        // Invertir dirección cuando llegue a ciertos límites
+        if (transform.position.x > 5)
+        {
+            direccion = -1; // Moverse hacia la izquierda
+        }
+        else if (transform.position.x < -5)
+        {
+            direccion = 1; // Moverse hacia la derecha
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 10f;
+    private int count;
+    private int totalPickUps; // Número total de elementos en el nivel
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+
+        // Contar cuántos objetos "PickUp" hay en la escena actual
+        totalPickUps = GameObject.FindGameObjectsWithTag("PickUp").Length;
+
+        if (countText != null)
+            setCountText();
+        else
+            Debug.LogWarning("countText no está asignado en el Inspector.");
+
+        if (winTextObject != null)
+            winTextObject.SetActive(false);
+        else
+            Debug.LogWarning("winTextObject no está asignado en el Inspector.");
+    }
+
+    void OnMove(InputValue movementVal)
+    {
+        Vector2 movementVector = movementVal.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void setCountText()
+    {
+        if (countText != null)
+        {
+            countText.text = $"Puntuación: {count} / {totalPickUps}";
+
+            if (count >= totalPickUps && winTextObject != null)
+                winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+}
